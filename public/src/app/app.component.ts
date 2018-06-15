b4333308abbaceaf66996b685cf12103bcbc262c
Jake <belica.j.92@gmail.com>
2018-06-15 13:19:27 -0500

    Updated app

---
+++ public/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  getImage() {
+    return "url('http://notgoaway.com/wp-content/uploads/2017/07/Dark-Background-61.jpg')"
+  }
+}
+
+
+
+//http://lapes.org/wp-content/uploads/2018/05/Html-Page-Background-Color-23-with-Html-Page-Background-Color.jpg
